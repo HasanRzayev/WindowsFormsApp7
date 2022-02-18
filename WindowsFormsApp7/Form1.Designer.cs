0581f227339948266182df682e83d835846bc0bc
Rzay_ri48@itstep.edu.az <Rzay_ri48@itstep.edu.az>
2022-02-18 21:47:24 +0400

    Add project files.

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_font = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox_size = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(45, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Font";
+            // 
+            // comboBox_font
+            // 
+            this.comboBox_font.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox_font.FormattingEnabled = true;
+            this.comboBox_font.Location = new System.Drawing.Point(25, 51);
+            this.comboBox_font.Name = "comboBox_font";
+            this.comboBox_font.Size = new System.Drawing.Size(372, 32);
+            this.comboBox_font.TabIndex = 1;
+            this.comboBox_font.SelectedIndexChanged += new System.EventHandler(this.comboBox_font_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(454, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Size";
+            // 
+            // comboBox_size
+            // 
+            this.comboBox_size.BackColor = System.Drawing.Color.Turquoise;
+            this.comboBox_size.FormattingEnabled = true;
+            this.comboBox_size.Location = new System.Drawing.Point(459, 60);
+            this.comboBox_size.Name = "comboBox_size";
+            this.comboBox_size.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_size.TabIndex = 3;
+            this.comboBox_size.SelectedIndexChanged += new System.EventHandler(this.comboBox_size_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(635, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Font Style";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(606, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 41);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "B";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(660, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 41);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "U";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(25, 98);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1381, 533);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(714, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 41);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "I";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(899, 48);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 41);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "R";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(845, 48);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(48, 41);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "C";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(791, 49);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(48, 41);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "L";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(820, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Alingment";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1005, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(1000, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Color";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1151, 9);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(48, 41);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "load";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1151, 56);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(48, 41);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "save";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1433, 660);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox_size);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox_font);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_font;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox_size;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           comboBox_size.Items.Add(8);
+
+           comboBox_size.Items.Add(9);
+      
+           comboBox_size.Items.Add(10);
+           
+           comboBox_size.Items.Add(11);
+           
+           comboBox_size.Items.Add(12);
+           
+           comboBox_size.Items.Add(14);
+           
+           comboBox_size.Items.Add(16);
+           
+           comboBox_size.Items.Add(18);
+           
+           comboBox_size.Items.Add(20);
+           
+           comboBox_size.Items.Add(22);
+           
+           comboBox_size.Items.Add(24);
+           
+           comboBox_size.Items.Add(26);
+           
+           comboBox_size.Items.Add(28);
+           
+           comboBox_size.Items.Add(36);
+           
+           comboBox_size.Items.Add(48);
+           
+           comboBox_size.Items.Add(72);
+
+            //      comboBox_font.DataSource = System.Drawing.FontFamily.Families.ToList();
+            //       comboBox1.DataSource = typeof(Color).GetProperties()
+            //.Where(x => x.PropertyType == typeof(Color))
+            //.Select(x => x.GetValue(null)).ToList();
+            foreach (System.Reflection.PropertyInfo prop in typeof(Color).GetProperties())
+            {
+                if (prop.PropertyType.FullName == "System.Drawing.Color")
+                    comboBox1.Items.Add(prop.Name);
+            }
+
+            //comboBox1.Items.Add("Red");
+            //comboBox1.SelectedItem = null;
+            //comboBox1.SelectedText = "Black";
+            //comboBox_font.SelectedItem = null;
+            //comboBox_font.SelectedText = "[FontFamily: Name=Arial Narrow]";
+            comboBox_size.SelectedItem = null;
+            comboBox_size.SelectedText = "8";
+        }
+
+        private void comboBox_font_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionFont = new Font(comboBox_font.Text, int.Parse(comboBox_size.Text), FontStyle.Bold);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionFont = new Font(comboBox_font.Text, int.Parse(comboBox_size.Text), FontStyle.Bold);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionFont = new Font(comboBox_font.Text, int.Parse(comboBox_size.Text), FontStyle.Italic);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            /*comboBox1.SelectedItem as string*/;
+
+
+            richTextBox1.ForeColor = Color.FromName((comboBox1.SelectedItem as  string));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionFont = new Font(comboBox_font.Text, int.Parse(comboBox_size.Text), FontStyle.Underline);
+        }
+
+        private void comboBox_size_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionFont = new Font(comboBox_font.Text, int.Parse(comboBox_size.Text), FontStyle.Underline);
+        }
+    }
+}
